63cd22a6f33195783311c38db7dae31524ab2a54
Thiago Vogt <63065574+Thiago-Vogt@users.noreply.github.com>
2025-02-14 18:06:28 -0300

    Arquivos Desafio

---
+++ TesteBenner/Network.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteBenner
+{
+    public class Network
+    {
+        private readonly int tamanho;
+        private readonly List<HashSet<int>> conexoes;
+
+        public Network(int tamanho)
+        {
+            if (tamanho < 0) {
+                throw new ArgumentException("Valor inválido, deve ser um número inteiro maior que 0");
+            }
+            this.tamanho = tamanho;
+            conexoes = new List<HashSet<int>>(tamanho + 1);
+            for (int i = 0; i <= tamanho; i++)
+            {
+                conexoes.Add(new HashSet<int>());
+            }
+        }
+
+        public void connect(int a , int b)
+        {
+            validarNumero(a);
+            validarNumero(b);
+
+            if(a == b)
+            {
+                throw new ArgumentException("Um número não pode se conectar com ele mesmo");
+            }
+
+            conexoes[a].Add(b);
+            conexoes[b].Add(a);
+        }
+
+        public void disconnect(int a, int b)
+        {
+            validarNumero(a);
+            validarNumero(b);
+
+            if (a == b)
+            {
+                throw new ArgumentException("Um número não pode se desconectar com ele mesmo");
+            }
+
+            if(!conexoes[a].Contains(b))
+            {
+                throw new Exception("Os números não estão conectados");
+            }
+
+            conexoes[a].Remove(b);
+            conexoes[b].Remove(a);
+
+        }
+
+        public Boolean query(int a, int b)
+        {
+            validarNumero(a);
+            validarNumero(b);
+
+            return levelConnection(a, b) > 0;
+        }
+
+        public int levelConnection(int a, int b)
+        {
+            validarNumero(a);
+            validarNumero(b);
+
+            if (conexoes[a].Contains(b)) return 1; //conexão direta
+
+            //Busca em largura para conexão indireta
+            var fila = new Queue<(int, int)>();
+            var visitado = new HashSet<int> { a };
+            fila.Enqueue((a, 1));
+
+            while (fila.Count > 0)
+            {
+                var (current, level) = fila.Dequeue();
+
+                foreach (var vizinho in conexoes[current])
+                {
+                    if (vizinho == b) return level;
+
+                    if (visitado.Add(vizinho))
+                    {
+                        fila.Enqueue((vizinho, level + 1));
+                    }
+                }
+            }
+
+            return 0; // Não há conexão entre os elementos
+        }
+
+        //valida se número está dentro tamnho setado
+        public void validarNumero(int num)
+        {
+            if (num < 0 || num > tamanho) {
+                throw new ArgumentOutOfRangeException("Numero fora do alcance");
+            }
+        }
+    }
+}
---
+++ TesteBenner/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TesteBenner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Network teste = new Network(8);
+            teste.connect(1, 2);
+            teste.connect(2,3);
+            teste.connect(3,4);
+            Console.WriteLine(teste.levelConnection(1,4));
+            Console.WriteLine(teste.query(1,5));
+            //teste.disconnect(1, 2);
+            //Console.WriteLine(teste.levelConnection(1, 2));
+        }
+    }
+}
